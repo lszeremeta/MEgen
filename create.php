09f5b368b8297b675966ab89ff4ee25741fe9a9a
Łukasz Szeremeta <l.szeremeta.dev@gmail.com>
2019-07-26 19:14:35 +0200

    Add support to other properties

--- create.php
+++ create.php
@@ -23,6 +23,11 @@ for ($i = 1; $i <= $count; $i++) {
   $identifier1 = isset($_POST["identifier-$i"]) ? $_POST["identifier-$i"] : '';
   $name1 = isset($_POST["name-$i"]) ? $_POST["name-$i"] : '';
   $inchikey1 = isset($_POST["inchikey-$i"]) ? $_POST["inchikey-$i"] : '';
+  $inchi1 = isset($_POST["inchi-$i"]) ? $_POST["inchi-$i"] : '';
+  $smiles1 = isset($_POST["smiles-$i"]) ? $_POST["smiles-$i"] : '';
+  $url1 = isset($_POST["url-$i"]) ? $_POST["url-$i"] : '';
+  $iupacname1 = isset($_POST["iupac-name-$i"]) ? $_POST["iupac-name-$i"] : '';
+  $molecularformula1 = isset($_POST["molecular-formula-$i"]) ? $_POST["molecular-formula-$i"] : '';
 
   $doc = $doc . "    <div typeof='schema:MolecularEntity' about='$subject1'>";
   if ($identifier1 != '') {
@@ -32,7 +37,22 @@ for ($i = 1; $i <= $count; $i++) {
     $doc = $doc . "\n      <div property='schema:name'>$name1</div>";
   }
   if ($inchikey1 != '') {
-    $doc = $doc . "\n      <div property='schema:name'>$inchikey1</div>";
+    $doc = $doc . "\n      <div property='schema:inChIKey'>$inchikey1</div>";
+  }
+  if ($inchi1 != '') {
+    $doc = $doc . "\n      <div property='schema:inChI'>$inchi1</div>";
+  }
+  if ($smiles1 != '') {
+    $doc = $doc . "\n      <div property='schema:smiles'>$smiles1</div>";
+  }
+  if ($url1 != '') {
+    $doc = $doc . "\n      <div property='schema:url'>$url1</div>";
+  }
+  if ($iupacname1 != '') {
+    $doc = $doc . "\n      <div property='schema:iupacName'>$iupacname1</div>";
+  }
+  if ($molecularformula1 != '') {
+    $doc = $doc . "\n      <div property='schema:molecularFormula'>$molecularformula1</div>";
   }
 
   $doc = $doc . "\n    </div>\n";    
